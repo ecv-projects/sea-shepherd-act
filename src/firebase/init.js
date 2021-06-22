2faf35c02bca9b77e13371d7ce2f0de8c90ffd75
Alice Mouchard <alice.mouchard85@gmail.com>
2021-06-22 10:42:00 +0200

    :wrench: Add firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: "AIzaSyBhH2tph_hSyt37yc2lpjQSldS--YuKcaU",
+  authDomain: "sea-shepherd-act-project.firebaseapp.com",
+  projectId: "sea-shepherd-act-project",
+  storageBucket: "sea-shepherd-act-project.appspot.com",
+  messagingSenderId: "749883795837",
+  appId: "1:749883795837:web:5ba440b47c62a625abc414"
 }
 
 firebase.initializeApp(config)
